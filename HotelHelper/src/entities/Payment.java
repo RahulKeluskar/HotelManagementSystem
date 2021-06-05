e6c4ac42470e28cfa661ebd815d6122749d4dcd1
Rahul <rahul.keluskar@sap.com>
2021-06-05 16:10:49 +0530

    Fixes

--- HotelHelper/src/entities/Payment.java
+++ HotelHelper/src/entities/Payment.java
@@ -1,6 +1,8 @@
 package entities;
 
-public class Payment {
+import java.io.Serializable;
+
+public class Payment implements Serializable {
     private String id;
     private String userId;
     private String upiAddress;
